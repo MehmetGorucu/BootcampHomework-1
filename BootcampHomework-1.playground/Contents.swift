4479b2bcee1ea423dcd66a99cda40192fd80138f
Mehmet <120107490+MehmetGorucu@users.noreply.github.com>
2023-01-06 23:14:11 +0300

    BootcampHomework-1
    Değişkenler

---
+++ BootcampHomework-1.playground/Contents.swift
@@ -0,0 +1,145 @@
+import UIKit
+
+import UIKit
+
+var sehir: String = "Adana"
+print(sehir)
+
+var ulke: String = "Hollanda"
+print("\(ulke)")
+
+var telefon: String = "+9 0 (506) 801 82 13"
+print("\(telefon)")
+
+var postaKodu: String = "34000"
+print(postaKodu)
+
+var email: String = "example@icloud.com"
+print(email)
+
+var meslek: String = "Avukat"
+print(meslek)
+
+var stokMiktarı: Int = 42
+print("\(stokMiktarı)")
+
+var müsteriAdi: String = "Mehmet"
+print(müsteriAdi)
+
+var bakiye: Int = 36
+print(bakiye)
+
+var dogumGunu: String = "9 Şubat"
+print(dogumGunu)
+
+var maas: Float = 8506.00
+print("2023 yılı için, Asgari ücret: \(maas)₺ olarak belirlendi")
+
+var medeniDurum: String = "Bekar"
+print(medeniDurum)
+
+var urunYorum: String = "Bu eğitim diğerlerine göre oldukça faydalı oldu."
+print(urunYorum)
+
+var odemeTarihi: String = "05.03.2023"
+print(odemeTarihi)
+
+var odeme: String = "Nakit"
+print("\(odeme)")
+
+var siparisAdeti: Int = 126
+print(siparisAdeti)
+
+var arabaModeli: String = "XC90"
+print("Volvo \(arabaModeli)")
+
+var kitapAdi: String = "Yeraltından Notlar"
+print(kitapAdi)
+
+var yayınlanmaTarihi: String = "Nisan 1864"
+print(yayınlanmaTarihi)
+
+var indirimMiktari: Int = 25
+print("% \(indirimMiktari) indirim")
+
+var odaSayisi: Int = 3
+print(odaSayisi)
+
+// İlk derste anlatılan şekilde kullanılmıştır
+
+var  enlem = (k1:36, k2:42)
+print("Türkiye ekvatorun kuzeyinde \(enlem) paralelleri arasında yer alır.")
+
+var boylam = (d1:26, d2:45)
+print("Türkiye \(boylam) doğu meridyenleri arasında yer alır.")
+
+var urunAdi: String = "Kulaklık"
+print(urunAdi)
+
+var yemekFiyatı: Double = 99.90
+print("Hesap: \(yemekFiyatı) ₺")
+
+var marka: String = "Cartier"
+print(marka)
+
+var muzikAdi: String = "Blues"
+print(muzikAdi)
+
+var videoSuresi: String = "25 Dakika"
+print(videoSuresi)
+
+var urunPuani: Float = 4.7
+print(urunPuani)
+
+var resimAdi: String = "The Scream"
+print(resimAdi)
+
+var dosyaFormati: String = "Pdf"
+print(dosyaFormati)
+
+var renk: String = "Kırmızı"
+print(renk)
+
+var renkKodu: String = "#FFA07A"
+print(renkKodu)
+
+var telefonModeli: String = "Venüz Z20"
+print(telefonModeli)
+
+var ekranBoyutu:String = "13 inç"
+print(ekranBoyutu)
+
+var agırlık: Double = 95.59
+print("\(agırlık) kilogram")
+
+var ulusalGun: String = "29 Ekim"
+print(ulusalGun)
+
+var tatilGunu: String = "Pazar"
+print(tatilGunu)
+
+var rezervasyonTarihi: String = "21 Şubat 2023"
+print(rezervasyonTarihi)
+
+var sokakAdi: String = "Kadife Sokak"
+print(sokakAdi)
+
+var otobusHatti: String = "500T"
+print(otobusHatti)
+
+var kalanDakika: Int = 28
+print("Kalan Süre: \(kalanDakika) dakika")
+
+var takipKodu: String = "192837465"
+print(takipKodu)
+
+var kuponSuresi: String = "3 Ay"
+print(kuponSuresi)
+
+var kuponKodu: String = "097815"
+print(kuponKodu)
+
+var faturaAdresi = ("Altunizade Mahallesi", "Çeken Sokak", "No:01", "Üsküdar", "İstanbul")
+var (mahalle, sokak, numara, ilce, sehir1) = faturaAdresi
+print("Adres: \(faturaAdresi.0), \(faturaAdresi.1), \(faturaAdresi.2), \(faturaAdresi.3) / \(faturaAdresi.4.uppercased())")
+
